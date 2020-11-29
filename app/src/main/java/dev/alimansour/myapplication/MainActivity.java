10b891f8c8a58ef6e3c980e3bcc264c6f84dd0fb
Ali Mansour <dev.ali.mansour@gmail.com>
2020-11-29 21:25:53 +0200

    Activity Lifecycle explanation

--- app/src/main/java/dev/alimansour/myapplication/MainActivity.java
+++ app/src/main/java/dev/alimansour/myapplication/MainActivity.java
@@ -18,6 +18,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Toast.makeText(this, "onCreate", Toast.LENGTH_LONG).show();
+
         welcomeButton = findViewById(R.id.welcomeButton);
         nameEditText = findViewById(R.id.nameEditText);
         clearButton = findViewById(R.id.clearButton);
@@ -27,12 +29,54 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     @Override
+    protected void onResume() {
+        super.onResume();
+        Toast.makeText(this, "onResume", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Toast.makeText(this, "onStart", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Toast.makeText(this, "onStop", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Toast.makeText(this, "onPause", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Toast.makeText(this, "onDestroy", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Toast.makeText(this, "onRestart", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
     public void onClick(View v) {
         if (v == welcomeButton) {
             String fullName = nameEditText.getText().toString();
-            Toast.makeText(this, "Welcome back " + fullName, Toast.LENGTH_LONG).show();
+            String message;
+            if (fullName.isEmpty())
+                message = getString(R.string.full_name_hint);
+            else message = getString(R.string.welcome_back) + " " + fullName;
+            Toast.makeText(this, message, Toast.LENGTH_LONG).show();
+            String resource = getString(R.string.test_resource);
         } else if (v == clearButton) {
             nameEditText.setText("");
+            Toast.makeText(this, getString(R.string.full_name_cleared_alert), Toast.LENGTH_LONG).show();
         }
     }
 }
